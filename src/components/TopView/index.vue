1f7b2afeee1ecd95d86919889a0b6ce24dd32fb8
AshtarteKk <865189421@qq.com>
2022-12-14 21:55:00 +0800

    完成顶部布局

--- src/components/TopView/index.vue
+++ src/components/TopView/index.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        TopView
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <el-card shadow="hover">鼠标悬浮时显示</el-card>
+            </el-col>
+            <el-col :span="6">
+                <el-card shadow="hover">鼠标悬浮时显示</el-card>
+            </el-col>
+            <el-col :span="6">
+                <el-card shadow="hover">鼠标悬浮时显示</el-card>
+            </el-col>
+            <el-col :span="6">
+                <el-card shadow="hover">鼠标悬浮时显示</el-card>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
