4362ae025dab57a144fcd1d6c32af7e198da27b4
AshtarteKk <865189421@qq.com>
2022-12-14 21:47:07 +0800

    完成容器布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="home">
     Home page
+    <TopView />
+    <SalesView />
+    <BottomView />
+    <MapView />
   </div>
 </template>
 
 <script>
-
+import TopView from '../components/TopView'
+import SalesView from '../components/SalesView'
+import BottomView from '../components/BottomView'
+import MapView from '../components/MapView'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: { TopView, SalesView, BottomView, MapView }
 }
 </script>
 
 <style lang="scss">
-
+.home {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: #eee;
+  box-sizing: border-box;
+}
 </style>
