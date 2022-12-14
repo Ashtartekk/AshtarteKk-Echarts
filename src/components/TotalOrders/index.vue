976334b1ea7e4b9c473f786798e62817497ef281
AshtarteKk <865189421@qq.com>
2022-12-14 23:11:01 +0800

    echart图表不显示bug未解决

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -1,7 +1,7 @@
 <template>
     <CommonCard title="累计订单量" value="￥ 2,157,420">
         <template>
-            <div id="total-orders-chart"></div>
+            <div id="total-orders-chart" style="width=50px;height=50px;"></div>
         </template>
         <template v-slot:footer>
             <span>昨日订单量</span>
@@ -19,7 +19,7 @@ export default {
         // 获取div的id
         const chartDom = document.getElementById('total-orders-chart')
         // 完成Echarts的初始化
-        const chart = this.$echarts.init(chartDom)
+        const chart = this.$echarts.init(chartDom, 'light', { render: 'svg' })
         chart.setOption({
             xAxis: {},
             yAxis: {},
