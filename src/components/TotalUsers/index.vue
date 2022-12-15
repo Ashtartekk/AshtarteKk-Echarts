d44a650d00204675a892d758257585e5f2ae21b3
AshtarteKk <865189421@qq.com>
2022-12-15 21:39:37 +0800

    完成累计用户数的数值

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <CommonCard></CommonCard>
+    <CommonCard title="累计用户数" value="1,087,503">
+        <template v-slot:footer>
+            <div class="total-users-footer">
+                <span>日同比</span>
+                <span class="emphasis">8.73%</span>
+                <div class="increase"></div>
+                <span>月同比</span>
+                <span class="emphasis">8.73%</span>
+                <div class="decrease"></div>
+            </div>
+        </template>
+    </CommonCard>
 </template>
 
 <script>
@@ -10,6 +21,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.total-users-footer {
+    display: flex;
 
+    .increase {
+        width: 0;
+        height: 0;
+        border-width: 6px;
+        border-color: transparent transparent red transparent;
+        border-style: solid;
+        margin: 0 10px 3px 5px
+    }
+
+    .decrease {
+        width: 0;
+        height: 0;
+        border-width: 6px;
+        border-color: green transparent transparent transparent;
+        border-style: solid;
+        margin: 5px 0 0 5px
+    }
+}
 </style>
